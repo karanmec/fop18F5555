139f91636011647d3ce477753222a8c1aeb54843
karanmec <67448875+karanmec@users.noreply.github.com>
2023-03-26 16:59:18 +0400

    Create week4a.c

---
+++ week4a.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	float a = 1, b = 2, c = 3, sum1, sum3;
+	int x = 1, y = 2, z = 3, sum2;
+	sum1 = a * b / c;
+	sum2 = (x + y % z * 5 - y);
+	sum3 = b - a / c * 4;
+	printf("Sum1 = %f\n", sum1);
+	printf("Sum2 = %d\n", sum2);
+	printf("Sum3 = %f", sum3);
+}
